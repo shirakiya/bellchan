fd9fa41d80db5c7b2767c7a0bd971eb061fc52e0
shirakiya <shirakiya.pv@gmail.com>
2017-07-15 16:45:28 +0900

    change message

--- bellchan/scheduled_plugins/money_forward.py
+++ bellchan/scheduled_plugins/money_forward.py
@@ -7,22 +7,23 @@ def notify_budget(bot, schedule):
         money_forward = MoneyForward()
         budget_status = money_forward.get_budget()
 
-        text = '今月で使った金額だよ！\n'
+        text = '予算報告だよ！\n'
+        text += '今月で使った金額\n'
         text += '【サマリー】\n'
-        text += f'*総額*: {budget_status.total_amount}円\n'
-        text += f'*変動費*: {budget_status.variable_amount}円\n'
+        text += f'- *総額*: {budget_status.total_amount}円\n'
+        text += f'- *変動費*: {budget_status.variable_amount}円\n'
         text += '【個別品目】\n'
         for variable_record in budget_status.variable_records:
-            text += f'*{variable_record.name}*: {variable_record.amount}円\n'
+            text += f'- *{variable_record.name}*: {variable_record.amount}円\n'
         text += '\n'
 
         text += '今月で残り使える金額だよ！\n'
         text += '【サマリー】\n'
-        text += f'*総額*: {budget_status.total_remaining}円\n'
-        text += f'*変動費*: {budget_status.variable_remaining}円\n'
+        text += f'- *総額*: {budget_status.total_remaining}円\n'
+        text += f'- *変動費*: {budget_status.variable_remaining}円\n'
         text += '【個別品目】\n'
         for variable_record in budget_status.variable_records:
-            text += f'*{variable_record.name}*: {variable_record.remaining}円\n'
+            text += f'- *{variable_record.name}*: {variable_record.remaining}円\n'
 
         bot.push_message(text)
 
