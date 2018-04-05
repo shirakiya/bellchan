357c0e8c9c07f3d52e14509f599a2c44177b8f0f
shirakiya <shirakiya.pv@gmail.com>
2018-04-06 02:12:11 +0900

    kill zombie process derived from parallel

--- bellchan/utils/work.py
+++ bellchan/utils/work.py
@@ -1,6 +1,15 @@
 from multiprocessing import Process
+import time
 
 
 def parallel(job_function):
     job_process = Process(target=job_function)
     job_process.start()
+
+    while True:
+        if job_process.is_alive():
+            time.sleep(1)
+        else:
+            break
+
+    job_process.terminate()
