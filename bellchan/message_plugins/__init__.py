651929e602ee00eb99accf988fa88d43ca268097
shirakiya <shirakiya.pv@gmail.com>
2019-10-02 00:08:33 +0900

    fix for flake8

--- bellchan/message_plugins/__init__.py
+++ bellchan/message_plugins/__init__.py
@@ -1,10 +1,9 @@
-from .restart import restart
 from .ping import ping
+from .restart import restart
 from .tokyo_dome_schedule import tokyo_dome_schedule
 
-
 __all__ = [
-    restart,
     ping,
+    restart,
     tokyo_dome_schedule,
 ]
